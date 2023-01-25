2062ef218bde7e25a602f56780115400ac158ca7
200-0k <faisal.s.alnamlah@gmail.com>
2023-01-26 01:24:57 +0300

    header + content + footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,107 @@
+<div class="bg-base-200 text-base-content">
+  <footer class="footer mx-auto max-w-6xl px-4 py-20">
+    <div>
+       <div class="font-title inline-flex text-3xl">
+          <span class="text-primary capitalize">Phone</span>
+          <span class="text-base-content capitalize">App</span>
+       </div>
+       <p class="opacity-50 text-center" dir="auto">
+          {!! wordwrap(__("app.summary"), 70, "<br />") !!}
+       </p>
+    </div>
+    <div>
+       <span class="footer-title">Guides</span>
+       <a href="#" class="link link-hover">How to install</a>
+       <a href="#" class="link link-hover">How to use components</a>
+       <a href="#" class="link link-hover">How to customize</a>
+       <a href="#" class="link link-hover">How to use themes</a>
+       <a href="#" class="link link-hover">Color system</a>
+       <a href="#" class="link link-hover">Config</a>
+    </div>
+    <div>
+       <span class="footer-title">Links</span>
+       <a target="_blank" rel="noopener" href="#" class="group link link-hover">
+          Github
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+             class="inline-block w-4 fill-current opacity-0 group-hover:opacity-50">
+             <path fill-rule="evenodd"
+                d="M19,14 L19,19 C19,20.1045695 18.1045695,21 17,21 L5,21 C3.8954305,21 3,20.1045695 3,19 L3,7 C3,5.8954305 3.8954305,5 5,5 L10,5 L10,7 L5,7 L5,19 L17,19 L17,14 L19,14 Z M18.9971001,6.41421356 L11.7042068,13.7071068 L10.2899933,12.2928932 L17.5828865,5 L12.9971001,5 L12.9971001,3 L20.9971001,3 L20.9971001,11 L18.9971001,11 L18.9971001,6.41421356 Z">
+             </path>
+          </svg>
+       </a>
+       <a href="#" class="group link link-hover">Changelog</a>
+       <a target="_blank" rel="noopener" href="#"
+          class="group link link-hover">
+          License
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+             class="inline-block w-4 fill-current opacity-0 group-hover:opacity-50">
+             <path fill-rule="evenodd"
+                d="M19,14 L19,19 C19,20.1045695 18.1045695,21 17,21 L5,21 C3.8954305,21 3,20.1045695 3,19 L3,7 C3,5.8954305 3.8954305,5 5,5 L10,5 L10,7 L5,7 L5,19 L17,19 L17,14 L19,14 Z M18.9971001,6.41421356 L11.7042068,13.7071068 L10.2899933,12.2928932 L17.5828865,5 L12.9971001,5 L12.9971001,3 L20.9971001,3 L20.9971001,11 L18.9971001,11 L18.9971001,6.41421356 Z">
+             </path>
+          </svg>
+       </a>
+       <a target="_blank" rel="noopener" href="#"
+          class="group link link-hover">
+          Contributing
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+             class="inline-block w-4 fill-current opacity-0 group-hover:opacity-50">
+             <path fill-rule="evenodd"
+                d="M19,14 L19,19 C19,20.1045695 18.1045695,21 17,21 L5,21 C3.8954305,21 3,20.1045695 3,19 L3,7 C3,5.8954305 3.8954305,5 5,5 L10,5 L10,7 L5,7 L5,19 L17,19 L17,14 L19,14 Z M18.9971001,6.41421356 L11.7042068,13.7071068 L10.2899933,12.2928932 L17.5828865,5 L12.9971001,5 L12.9971001,3 L20.9971001,3 L20.9971001,11 L18.9971001,11 L18.9971001,6.41421356 Z">
+             </path>
+          </svg>
+       </a>
+    </div>
+    <div>
+       <span class="footer-title">Available on</span>
+       <a target="_blank" rel="noopener" href="#" class="group link link-hover">
+          NPM
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+             class="inline-block w-4 fill-current opacity-0 group-hover:opacity-50">
+             <path fill-rule="evenodd"
+                d="M19,14 L19,19 C19,20.1045695 18.1045695,21 17,21 L5,21 C3.8954305,21 3,20.1045695 3,19 L3,7 C3,5.8954305 3.8954305,5 5,5 L10,5 L10,7 L5,7 L5,19 L17,19 L17,14 L19,14 Z M18.9971001,6.41421356 L11.7042068,13.7071068 L10.2899933,12.2928932 L17.5828865,5 L12.9971001,5 L12.9971001,3 L20.9971001,3 L20.9971001,11 L18.9971001,11 L18.9971001,6.41421356 Z">
+             </path>
+          </svg>
+       </a>
+       <a target="_blank" rel="noopener" href="#" class="group link link-hover">
+          Unpkg CDN
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+             class="inline-block w-4 fill-current opacity-0 group-hover:opacity-50">
+             <path fill-rule="evenodd"
+                d="M19,14 L19,19 C19,20.1045695 18.1045695,21 17,21 L5,21 C3.8954305,21 3,20.1045695 3,19 L3,7 C3,5.8954305 3.8954305,5 5,5 L10,5 L10,7 L5,7 L5,19 L17,19 L17,14 L19,14 Z M18.9971001,6.41421356 L11.7042068,13.7071068 L10.2899933,12.2928932 L17.5828865,5 L12.9971001,5 L12.9971001,3 L20.9971001,3 L20.9971001,11 L18.9971001,11 L18.9971001,6.41421356 Z">
+             </path>
+          </svg>
+       </a>
+       <a target="_blank" rel="noopener" href="#"
+          class="group link link-hover">
+          JSdeliver CDN
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+             class="inline-block w-4 fill-current opacity-0 group-hover:opacity-50">
+             <path fill-rule="evenodd"
+                d="M19,14 L19,19 C19,20.1045695 18.1045695,21 17,21 L5,21 C3.8954305,21 3,20.1045695 3,19 L3,7 C3,5.8954305 3.8954305,5 5,5 L10,5 L10,7 L5,7 L5,19 L17,19 L17,14 L19,14 Z M18.9971001,6.41421356 L11.7042068,13.7071068 L10.2899933,12.2928932 L17.5828865,5 L12.9971001,5 L12.9971001,3 L20.9971001,3 L20.9971001,11 L18.9971001,11 L18.9971001,6.41421356 Z">
+             </path>
+          </svg>
+       </a>
+    </div>
+    <div>
+       <span class="footer-title">Try online</span>
+       <a href="#" class="group link link-hover" target="_blank" rel="noopener">
+          Tailwind Play
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+             class="inline-block w-4 fill-current opacity-0 group-hover:opacity-50">
+             <path fill-rule="evenodd"
+                d="M19,14 L19,19 C19,20.1045695 18.1045695,21 17,21 L5,21 C3.8954305,21 3,20.1045695 3,19 L3,7 C3,5.8954305 3.8954305,5 5,5 L10,5 L10,7 L5,7 L5,19 L17,19 L17,14 L19,14 Z M18.9971001,6.41421356 L11.7042068,13.7071068 L10.2899933,12.2928932 L17.5828865,5 L12.9971001,5 L12.9971001,3 L20.9971001,3 L20.9971001,11 L18.9971001,11 L18.9971001,6.41421356 Z">
+             </path>
+          </svg>
+       </a>
+       <a target="_blank" rel="noopener" href="#" class="group link link-hover">
+          Codepen
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+             class="inline-block w-4 fill-current opacity-0 group-hover:opacity-50">
+             <path fill-rule="evenodd"
+                d="M19,14 L19,19 C19,20.1045695 18.1045695,21 17,21 L5,21 C3.8954305,21 3,20.1045695 3,19 L3,7 C3,5.8954305 3.8954305,5 5,5 L10,5 L10,7 L5,7 L5,19 L17,19 L17,14 L19,14 Z M18.9971001,6.41421356 L11.7042068,13.7071068 L10.2899933,12.2928932 L17.5828865,5 L12.9971001,5 L12.9971001,3 L20.9971001,3 L20.9971001,11 L18.9971001,11 L18.9971001,6.41421356 Z">
+             </path>
+          </svg>
+       </a>
+    </div>
+ </footer>
+</div>
