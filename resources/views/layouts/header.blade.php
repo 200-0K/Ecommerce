e8072242aca8ba8065ea7cf3002a0d32edf4ba3b
200-0k <faisal.s.alnamlah@gmail.com>
2023-02-05 23:36:19 +0300

    enhance header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,34 +3,38 @@
     <a href="/" class="btn btn-ghost normal-case text-xl">{{ config('app.name') }}</a>
   </div>
   <div class="flex-none">
+    {{-- Navigation Links --}}
     <div class="border-r-2 mr-2">
       <a href="{{ route('explore') }}" class="btn btn-ghost normal-case">{{ __('app.explore') }}</a>
     </div>
+    
+    {{-- Cart --}}
     <div class="dropdown dropdown-end">
       <label tabindex="0" class="btn btn-ghost btn-circle">
         <div class="indicator">
-          <svg width="24px" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <a {{ !Auth::check() ? 'href='.route('login').'' : '' }}>
+            <svg width="24px" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-          {{-- <span class="badge badge-sm indicator-item">TODO: get cart items for current user from db</span> --}}
+          </a>
+          @auth
+          <span class="badge badge-sm indicator-item">{{ Auth::user()->cart->count() ?? '' }}</span>
+          @endauth
         </div>
       </label>
+      @auth
       <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-200 shadow">
         <div class="card-body">
-          <span class="font-bold text-lg">{{-- TODO: get cart items for current user from db --}}</span>
-          <span class="text-info">{{-- TODO: get cart items total price for current user from db --}}</span>
+          {{-- <span class="font-bold text-lg">{{ Auth::user()->cart->count() ?? '' }}</span> --}}
+          <span class="text-info" dir="auto">{{ Auth::user()->cart->sum(fn ($product) => $product->new_price ?? $product->price ) ?? '' }} ريال</span>
           <div class="card-actions">
-            @auth
-            @if(Auth::user()?->cart()->count() > 0)
             <a href="{{route('checkout')}}" class="btn btn-primary btn-block">{{ __('app.view_cart') }}</a>
-            @endif
-            @else
-            <a href="{{route('login')}}" class="btn btn-primary btn-block">{{ __('auth.login_required') }}</a>
-            @endauth
           </div>
         </div>
       </div>
+      @endauth
     </div>
 
+    {{-- Profile --}}
     <x-dropdown width="48">
       <x-slot name="trigger">
         <label tabindex="0" class="btn btn-ghost btn-circle avatar">
