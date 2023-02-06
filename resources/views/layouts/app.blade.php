df84035a0e209e25006049df6a5e6da59e2ebda3
200-0k <faisal.s.alnamlah@gmail.com>
2023-02-06 20:09:26 +0300

    meta tags | resolves #1, resolves #2, resolves #5

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,13 +2,21 @@
 @php
 $title = ($title ?? false) ? $title.' - ' : '';
 $title .= config('app.name');
+$lang = str_replace('_', '-', app()->getLocale());
 @endphp
 <!DOCTYPE html>
-<html dir="{{ __('app.page_dir') == 'rtl' ? 'rtl' : 'ltr' }}" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html dir="{{ __('app.page_dir') == 'rtl' ? 'rtl' : 'ltr' }}" lang="{{ $lang }}">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="keywords" content="mobile phones, ecommerce, latest models, budget-friendly, fast shipping, easy returns">
+  <meta name="description" content="{{ __('app.summary') }}">
+  <meta name="robots" content="index, follow">
+  <meta name="author" content="{{ config('app.name') }}">
+  <meta name="revisit-after" content="7 days">
+  <meta name="language" content="{{ $lang }}">
+  <meta name="googlebot" content="noodp">
 
   <title>{{ $title }}</title>
 
